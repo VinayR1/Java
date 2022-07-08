01260377398fca894d5fa5f30606ed49a058b00c
dondavinayreddy@gmail.com <39482416+VinayR1@users.noreply.github.com>
2022-07-08 13:39:44 -0500

    Create Booleantest.java

---
+++ Booleantest.java
@@ -0,0 +1,21 @@
+/* Online Java Compiler and Editor */
+public class BooleanTest
+{
+    public static void main(String args[])
+    {
+        /*
+        In java we cannot do a boolean test on an integer
+        int a = 1;
+        while(a)
+        {
+            system.out.println("Hello World");
+        } */
+        boolean isHot = true; 
+        while(isHot)
+        {
+            System.out.println("Hello World");
+            break;
+        }
+        
+    }
+}
