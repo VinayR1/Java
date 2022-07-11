e8bdcaa2b4bbf4478875ba3dd1b056ef83fbee6e
dondavinayreddy@gmail.com <39482416+VinayR1@users.noreply.github.com>
2022-07-11 15:34:40 -0500

    Create Arraysbasics.java

---
+++ Arraysbasics.java
@@ -0,0 +1,12 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		int[] ArrayOfNumbers = {1,2,3,4,5,6,7};
+		for(int i=0; i< ArrayOfNumbers.length; i++)
+		{
+		    System.out.println(ArrayOfNumbers[i]);
+		}
+		
+	}
+}
